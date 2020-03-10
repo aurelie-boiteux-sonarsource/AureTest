4b34ceb5d8c863fe783065989e78558bd8c16a59
Aurelie Boiteux Cabourdin <42917701+aurelie-boiteux-sonarsource@users.noreply.github.com>
2020-03-10 11:38:30 +0100

    Update App.java

--- src/main/java/com/jcg/maven/App.java
+++ src/main/java/com/jcg/maven/App.java
@@ -14,5 +14,41 @@ public class App
     public void notCovered() {
         System.out.println("notCovered");
     }
+    
+     private static final int MAX_PRICE = 10;
+    public void setPriceGitChecks(int price) {
+        assert price >= 0 && price <= MAX_PRICE;
+        // Set the price
+        //TODO blabla
+    }
+
+
+
+
+    public void aMethod(){
+        int i=0;
+        while (i<10){
+            i++;
+        }}
+
+
+    class A {
+        void foo(boolean a, Object b) throws MyException {
+            Object o = bar(a, b);
+            if (a) {
+                o.toString(); // HERE
+            }
+        }
+        private Object bar(boolean b, Object o) throws MyException {
+            if (b) {
+                return null;
+            }
+            if (o == null)  {
+                throw new MyException();
+            }
+            return o;
+        }
+    }
+    class MyException extends Exception { }
 
 }
